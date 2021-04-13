14a257f441a2f49309de241975fbc65858cda9c2
Guilherme Souza <gui_csouza@outlook.com>
2021-04-12 22:24:37 -0300

    add meters page

--- assets/m-page/m.js
+++ assets/m-page/m.js
@@ -11,7 +11,7 @@
     var c = str / 100;
     var d = str * 10
 ;
-    var e = "Feet:  " + a + "  ft" + "<br>" + "Kilometers:  " + b + "  Km" + "<br>" + "Meters:  " +  c + "  m" + "<br>" + "Milimeters:  " +  d + "  mm";
+    var e = "Feet:  " + a + "  ft" + "<br>" + "Kilometers:  " + b + "  Km" + "<br>" + "Centimeters:  " +  c + "  m" + "<br>" + "Milimeters:  " +  d + "  mm";
     document.getElementById("demo").innerHTML=e;
 
     
