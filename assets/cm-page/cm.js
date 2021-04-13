f4d4cb5264c0f8f2e896cef5b69c51869bc5e50a
Guilherme Souza <gui_csouza@outlook.com>
2021-04-12 22:14:18 -0300

    add cm page

--- assets/cm-page/cm.js
+++ assets/cm-page/cm.js
@@ -5,13 +5,14 @@
     
     
     var str = +document.getElementById("demoInput").value;
-    var a = str.toString();
-    var b = str.toString(2);
+    var a = str / 30.48;
+    var b = str /100000;
     
-    var c = str.toString(8);
-    var d = str.toString(16);
+    var c = str / 100;
+    var d = str * 10
+;
     var d2 = d.toUpperCase();
-    var e = "Decimal Number:  " + a + "<br>" + "Binary:  " + b + "<br>" + "Octal:  " +  c + "<br>" + "Hexadecimal:  " +  d2;
+    var e = "Feet:  " + a + "<br>" + "Kilometers:  " + b + "<br>" + "Meters:  " +  c + "<br>" + "Milimeters:  " +  d2;
     document.getElementById("demo").innerHTML=e;
 
     
